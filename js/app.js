cd0b4987e19f274339046576042429fdda693c8a
CRB404 <adamlukasik@adams-mbp-2.ideo.com>
2016-11-17 14:01:10 -0800

    3 node demo
    all calls active (untested)

--- js/app.js
+++ js/app.js
@@ -3,7 +3,47 @@ var particle = new Particle();
 particle.login({username: 'weftech@gmail.com', password: 'W3Ftech@spark'});
 
 
-function trigerLight(){
-  particle.callFunction({ deviceId: 'DEVICE_ID', name: 'FUNCTION_NAME', argument: 'STRING TO FUNCTION', auth: 'AUTH_TOKEN' });
-  console.log('triggered')
+function greenSmall(){
+  particle.callFunction({ deviceId: '3c0039000c47343432313031', name: 'TableNote', argument: 'greenSmall', auth: '7dfb755486e5e116bf9a73c6764959c1180c64cf' });
+  console.log('greenSmall')
+}
+
+function greenMed(){
+  particle.callFunction({ deviceId: '3c0039000c47343432313031', name: 'TableNote', argument: 'greenMed', auth: '7dfb755486e5e116bf9a73c6764959c1180c64cf' });
+  console.log('greenMed')
+}
+
+function greenLarge(){
+  particle.callFunction({ deviceId: '3c0039000c47343432313031', name: 'TableNote', argument: 'greenLarge', auth: '7dfb755486e5e116bf9a73c6764959c1180c64cf' });
+  console.log('greenLarge')
+}
+
+function yellowSmall(){
+  particle.callFunction({ deviceId: '3c0039000c47343432313031', name: 'TableNote', argument: 'yellowSmall', auth: '7dfb755486e5e116bf9a73c6764959c1180c64cf' });
+  console.log('yellowSmall')
+}
+
+function yellowMed(){
+  particle.callFunction({ deviceId: '3c0039000c47343432313031', name: 'TableNote', argument: 'yellowMed', auth: '7dfb755486e5e116bf9a73c6764959c1180c64cf' });
+  console.log('yellowMed')
+}
+
+function yellowLarge(){
+  particle.callFunction({ deviceId: '3c0039000c47343432313031', name: 'TableNote', argument: 'yellowLarge', auth: '7dfb755486e5e116bf9a73c6764959c1180c64cf' });
+  console.log('yellowLarge')
+}
+
+function redSmall(){
+  particle.callFunction({ deviceId: '3c0039000c47343432313031', name: 'TableNote', argument: 'redSmall', auth: '7dfb755486e5e116bf9a73c6764959c1180c64cf' });
+  console.log('redSmall')
+}
+
+function redMed(){
+  particle.callFunction({ deviceId: '3c0039000c47343432313031', name: 'TableNote', argument: 'redMed', auth: '7dfb755486e5e116bf9a73c6764959c1180c64cf' });
+  console.log('redMed')
+}
+
+function redLarge(){
+  particle.callFunction({ deviceId: '3c0039000c47343432313031', name: 'TableNote', argument: 'redLarge', auth: '7dfb755486e5e116bf9a73c6764959c1180c64cf' });
+  console.log('redLarge')
 }
