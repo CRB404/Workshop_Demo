7f14774887ddf565b25d7fb430fe79f85317bbe6
CRB404 <adamlukasik@Adams-MacBook-Pro-2.local>
2016-11-30 17:46:59 -0800

    update to match incoming syntax from workshop cafe controller
    syntax matched to the ruby controller that runs workshop cafe

--- js/app.js
+++ js/app.js
@@ -4,46 +4,46 @@ particle.login({username: 'weftech@gmail.com', password: 'W3Ftech@spark'});
 
 
 function greenSmall(){
-  particle.callFunction({ deviceId: '23003c000947343432313031', name: 'TableNote', argument: 'greenSmall', auth: '7dfb755486e5e116bf9a73c6764959c1180c64cf' });
+  particle.callFunction({ deviceId: '23003c000947343432313031', name: 'TableNote', argument: 'green112', auth: '7dfb755486e5e116bf9a73c6764959c1180c64cf' });
   console.log('greenSmall')
 }
 
 function greenMed(){
-  particle.callFunction({ deviceId: '23003c000947343432313031', name: 'TableNote', argument: 'greenMed', auth: '7dfb755486e5e116bf9a73c6764959c1180c64cf' });
+  particle.callFunction({ deviceId: '23003c000947343432313031', name: 'TableNote', argument: 'green111', auth: '7dfb755486e5e116bf9a73c6764959c1180c64cf' });
   console.log('greenMed')
 }
 
 function greenLarge(){
-  particle.callFunction({ deviceId: '23003c000947343432313031', name: 'TableNote', argument: 'greenLarge', auth: '7dfb755486e5e116bf9a73c6764959c1180c64cf' });
+  particle.callFunction({ deviceId: '23003c000947343432313031', name: 'TableNote', argument: 'green110', auth: '7dfb755486e5e116bf9a73c6764959c1180c64cf' });
   console.log('greenLarge')
 }
 
 function yellowSmall(){
-  particle.callFunction({ deviceId: '23003c000947343432313031', name: 'TableNote', argument: 'yellowSmall', auth: '7dfb755486e5e116bf9a73c6764959c1180c64cf' });
+  particle.callFunction({ deviceId: '23003c000947343432313031', name: 'TableNote', argument: 'yellow112', auth: '7dfb755486e5e116bf9a73c6764959c1180c64cf' });
   console.log('yellowSmall')
 }
 
 function yellowMed(){
-  particle.callFunction({ deviceId: '23003c000947343432313031', name: 'TableNote', argument: 'yellowMed', auth: '7dfb755486e5e116bf9a73c6764959c1180c64cf' });
+  particle.callFunction({ deviceId: '23003c000947343432313031', name: 'TableNote', argument: 'yellow111', auth: '7dfb755486e5e116bf9a73c6764959c1180c64cf' });
   console.log('yellowMed')
 }
 
 function yellowLarge(){
-  particle.callFunction({ deviceId: '23003c000947343432313031', name: 'TableNote', argument: 'yellowLarge', auth: '7dfb755486e5e116bf9a73c6764959c1180c64cf' });
+  particle.callFunction({ deviceId: '23003c000947343432313031', name: 'TableNote', argument: 'yellow110', auth: '7dfb755486e5e116bf9a73c6764959c1180c64cf' });
   console.log('yellowLarge')
 }
 
 function redSmall(){
-  particle.callFunction({ deviceId: '23003c000947343432313031', name: 'TableNote', argument: 'redSmall', auth: '7dfb755486e5e116bf9a73c6764959c1180c64cf' });
+  particle.callFunction({ deviceId: '23003c000947343432313031', name: 'TableNote', argument: 'red112', auth: '7dfb755486e5e116bf9a73c6764959c1180c64cf' });
   console.log('redSmall')
 }
 
 function redMed(){
-  particle.callFunction({ deviceId: '23003c000947343432313031', name: 'TableNote', argument: 'redMed', auth: '7dfb755486e5e116bf9a73c6764959c1180c64cf' });
+  particle.callFunction({ deviceId: '23003c000947343432313031', name: 'TableNote', argument: 'red111', auth: '7dfb755486e5e116bf9a73c6764959c1180c64cf' });
   console.log('redMed')
 }
 
 function redLarge(){
-  particle.callFunction({ deviceId: '23003c000947343432313031', name: 'TableNote', argument: 'redLarge', auth: '7dfb755486e5e116bf9a73c6764959c1180c64cf' });
+  particle.callFunction({ deviceId: '23003c000947343432313031', name: 'TableNote', argument: 'red110', auth: '7dfb755486e5e116bf9a73c6764959c1180c64cf' });
   console.log('redLarge')
 }
